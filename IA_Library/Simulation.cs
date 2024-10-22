19f38fc14a41dae834a63176af3f73dbdd36b0f8
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2024-10-22 14:50:58 -0300

    WIP Dll

---
+++ IA_Library/Simulation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IA_Library
+{
+    public class Simulation
+    {
+
+        public void StartSimulation()
+        {
+            
+        }
+
+        public void UpdateSimulation(float deltaTime)
+        {
+            
+        }
+
+        public void EndSimulation()
+        {
+            
+        }
+    }
+}
